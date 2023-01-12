628c5e5972598b398bf09780a5e8d482e952ee5a
Ronald Garcia <ronaldag58@gmail.com>
2023-01-12 09:57:57 -0300

    feat: add service layer

--- src/main/java/com/demo/course/resources/UserResource.java
+++ src/main/java/com/demo/course/resources/UserResource.java
@@ -1,19 +1,36 @@
 package com.demo.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.course.entities.User;
+import com.demo.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1l, "maria", "maria@gmail.com", "9999999", "1234");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = userService.findAll();
+		
+		return ResponseEntity.ok().body(users);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 }
